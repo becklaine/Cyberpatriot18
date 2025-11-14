1c70863108237d4970b165b461b7c2950018e797
becklaine <lelainabeck@gmail.com>
2025-11-13 18:21:45 -0800

    Update 2.sh

--- 2.sh
+++ 2.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+# MODIFY TO FIT YOUR CYBERPATRIOT IMAGE
+# Some things in here might not be okay according to the README
+# Look through BEFORE running
 echo Updating
 apt-get update -y
 apt-get upgrade -y
@@ -23,5 +26,5 @@ apt-get install libpam-cracklib
 echo Removing hacker tools
 apt purge wireshark* ophcrack* john* deluge* nmap* hydra*
 
-echo Enabling Automatic Updates (Daily)
+echo Enabling Automatic Updates
 apt install unattended-upgrades
